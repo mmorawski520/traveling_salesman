0d6618b4df33c38e2731d5fd950633a8ae2c0898
jp2137 <sbaiyassine2004@gmail.com>
2022-11-25 11:23:26 +0100

    added btnDelete

--- app/src/main/java/com/example/komiwojazer/OptionsActivity.kt
+++ app/src/main/java/com/example/komiwojazer/OptionsActivity.kt
@@ -1,33 +1,35 @@
 package com.example.komiwojazer
 
 import android.content.Intent
+import android.location.GnssAntennaInfo
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import java.lang.Math.random
 import kotlin.random.Random
 
 
 class OptionsActivity : AppCompatActivity() {
     lateinit var btnAdd:Button;
     lateinit var btnSave:Button;
+    lateinit var btnDelete:Button;
     lateinit var editTextCity:EditText;
     lateinit var editTextDistance:EditText;
-
+    lateinit var recyclerview: RecyclerView;
+    val data = ArrayList<CitiesViewModel>()
     lateinit var cities:Array<City>;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_options)
 
-
-
         btnAdd = findViewById(R.id.btnAdd)
         btnSave = findViewById(R.id.btnSave)
+        btnDelete = findViewById(R.id.btnDelete)
 
+        recyclerview = findViewById<RecyclerView>(R.id.recyclerView);
         editTextCity = findViewById(R.id.editTextCity)
         editTextDistance = findViewById(R.id.editTextDistance)
 
@@ -39,19 +41,21 @@ class OptionsActivity : AppCompatActivity() {
         btnAdd.setOnClickListener {
 
         }
+        btnDelete.setOnClickListener {
+
+        }
 
         generateRandomCities()
     }
+
     private fun generateRandomCities(){
-        val recyclerview = findViewById<RecyclerView>(R.id.recyclerView);
+
         recyclerview.layoutManager = LinearLayoutManager(this)
 
 
-        val data = ArrayList<CitiesViewModel>()
-
         for (i in 1..16) {
-
             data.add(CitiesViewModel("City " + i.toString(), Random.nextInt(1, 10)))
+
         }
 
         val adapter = CustomAdapter(data)
